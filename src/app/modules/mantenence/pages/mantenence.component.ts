53f77b370205060301a75a443be5726cd86dcde0
Julian-pixel366 <julianagrg4l@gmail.com>
2021-09-28 23:51:56 -0500

    validaciones en 90%

--- src/app/modules/mantenence/pages/mantenence.component.ts
+++ src/app/modules/mantenence/pages/mantenence.component.ts
@@ -1,3 +1,4 @@
+import { Validators, FormBuilder } from '@angular/forms';
 import { Component } from '@angular/core';
 
 
@@ -7,5 +8,21 @@ import { Component } from '@angular/core';
     styleUrls: ['./mantenence.component.scss']
 })
 export class MantenenceComponent {
+
+    MantenenceForm = this.fb.group({
+        name:['',[Validators.required, Validators.minLength(3)]],
+        email: ['', [Validators.required, Validators.email]],
+        check: ['',[Validators.required]],
+        made:['',[Validators.required ]],
+        phone:['',[Validators.required, Validators.minLength(3)]],
+        adress:['',[Validators.required, Validators.minLength(5)]],
+        city:['',[Validators.required, Validators.minLength(5)]],
+        idCity:['',[Validators.required]]
+    })
+    constructor(private fb: FormBuilder) { }
+
+    mantenence(){
+        console.log(this.MantenenceForm.value);
+    }
     
 }
