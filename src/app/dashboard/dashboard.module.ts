62dfd5e5a5a51c9449bafb586144bec15e6dd63b
Julian-pixel366 <julianagrg4l@gmail.com>
2021-11-07 22:05:10 -0500

    union

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,14 +1,36 @@
+import { ReactiveFormsModule } from '@angular/forms';
+import { FixedPluginModule } from './shared/fixedplugin/fixedplugin.module';
+import { FooterModule } from './shared/footer/footer.module';
+import { NavbarModule } from './shared/navbar/navbar.module';
+import { SidebarModule } from './sidebar/sidebar.module';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { AppComponent } from './../app.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AppComponent,
+    AdminLayoutComponent
+  ],
   imports: [
-    CommonModule,
-    DashboardRoutingModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(AppRoutes,{
+      
+      useHash: true
+    }),
+    SidebarModule,
+    NavbarModule,
+    ToastrModule.forRoot(),
+    FooterModule,
+    FixedPluginModule,
+    ReactiveFormsModule
   ]
 })
 export class DashboardModule { }
---
+++ src/app/dashboard/pages/user/user.component.ts
@@ -0,0 +1,14 @@
+import { style } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'user-cmp',
+    moduleId: module.id,
+    templateUrl: 'user.component.html',
+    styleUrls: ['./product.component.scss']
+})
+
+export class UserComponent implements OnInit{
+    ngOnInit(){
+    }
+}
