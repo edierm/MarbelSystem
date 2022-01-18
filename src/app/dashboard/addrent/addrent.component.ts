c076c2382514bef2057924d609a8ed982a35d6af
Andres Berruecos <andresbg446@gmail.com>
2022-01-18 17:53:28 -0500

    Hi

---
+++ src/app/dashboard/addrent/addrent.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'addrent-cmp',
+    
+    templateUrl: 'addrent.component.html',
+    styleUrls : ['./addrent.component.scss']
+})
+
+export class AddRentComponent{}
---
+++ src/app/dashboard/sales/components/addsail/addsail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'addsail-cmp',
+    
+    templateUrl: 'addsail.component.html',
+    styleUrls : ['./addsail.component.scss']
+})
+
+export class AddSailComponent{}
