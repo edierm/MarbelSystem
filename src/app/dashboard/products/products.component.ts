5dea7962d801289fabc5cc1c6d080dcc856b67b7
Julian-pixel366 <julianagrg4l@gmail.com>
2022-02-22 17:53:58 -0500

    productos

--- src/app/dashboard/products/products.component.ts
+++ src/app/dashboard/products/products.component.ts
@@ -1,5 +1,6 @@
+import { ModalDirective } from 'ngx-bootstrap/modal';
 import { Validators, FormBuilder } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ProductService } from 'src/services/products.service';
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -10,6 +11,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class ProductsDashComponent implements OnInit {
   imageSrc: string = '';
+  @ViewChild('modalSuccess') public modalRef: ModalDirective;
   productForm = this.fb.group({
     name: ['', [Validators.required]],
     image: ['', [Validators.required]],
@@ -41,7 +43,14 @@ export class ProductsDashComponent implements OnInit {
     }
     console.log(this.router.url);
   }
-
+  dataModal = {
+    title: '',
+    body: '',
+  };
+  openModal() {
+    this.modalRef.hide();
+    this.modalRef.show();
+  }
   handleInputChangeImg(e) {
     var file = e.dataTransfer ? e.dataTransfer.files[0] : e.target.files[0];
     var pattern = /image-*/;
@@ -70,11 +79,25 @@ export class ProductsDashComponent implements OnInit {
         console.log(this.route);
         this.productService.updateProduct(data).subscribe((res) => {
             console.log('Actualizado producto', res);
+            this.dataModal.title = 'Producto Actualizado';
+        this.dataModal.body = 'Exito';
+        this.openModal();
+      }, (error) => {
+        this.dataModal.title = 'Error';
+        this.dataModal.body = 'No se pudo Actualizar el producto';
+        this.openModal();
         });
     } else {
       console.log('Data Producto: ', data);
       this.productService.createProduct(data).subscribe((res) => {
         console.log('Creado producto', res);
+        this.dataModal.title = 'Producto Creado';
+        this.dataModal.body = 'Exito';
+        this.openModal();
+      }, (error) => {
+        this.dataModal.title = 'Error';
+        this.dataModal.body = 'No se pudo crear el producto';
+        this.openModal();
       });
     }
   }
