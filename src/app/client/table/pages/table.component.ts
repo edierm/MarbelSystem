d9447f610b59bbb8e2199c27f3baa22854ece623
Julian-pixel366 <julianagrg4l@gmail.com>
2022-02-25 16:29:29 -0500

    ultimo

--- src/app/client/table/pages/table.component.ts
+++ src/app/client/table/pages/table.component.ts
@@ -9,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit {
-    constructor(private productsService: ProductService,
+    constructor(
+      private productsService: ProductService,
       private router: Router,
-      private route: ActivatedRoute) {}
+      private route: ActivatedRoute
+      ) {}
     productos: [];
     ngOnInit() {
       this.productos = JSON.parse(sessionStorage.getItem('dataTable'));
