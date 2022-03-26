52c208d977bda38d87e7ed2bdde6773d479178dd
Julian-pixel366 <julianagrg4l@gmail.com>
2022-03-26 16:17:21 -0500

    pantalla

--- src/app/login/login/pages/login.component.ts
+++ src/app/login/login/pages/login.component.ts
@@ -1,8 +1,9 @@
+import { ModalDirective } from 'ngx-bootstrap/modal';
 import { UsersService } from './../../../../services/users.service';
 import { Router } from '@angular/router';
 import { environment } from './../../../../environments/environment';
 import { FormBuilder, Validators } from '@angular/forms';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -10,10 +11,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+
+  @ViewChild('modalSuccess') public modal: ModalDirective;
   loginForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(3)]],
   });
+  dataModal = {
+    title: '',
+    body: '',
+  };
+  openModal() {
+    this.modal.hide();
+    this.modal.show();
+  }
+
   constructor(
     private fb: FormBuilder,
     private userservices: UsersService,
@@ -29,11 +41,16 @@ export class LoginComponent {
             localStorage.setItem('userLogin', JSON.stringify(user));
             this.router.navigate(['/dashboard']);
         }
-        console.log(res);
+        console.log('Respuesta del back',res);
+
       },
       (error) => {
-        window.location.href = environment.urlDashborad;
+        /* window.location.href = environment.urlDashborad; */
         localStorage.setItem('userLogin', null);
+      
+        this.dataModal.title = 'ERROR';
+        this.dataModal.body = 'Correo o contraseña invalidos';
+        this.openModal();
       }
     );
   }
